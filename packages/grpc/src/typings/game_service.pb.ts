13c5582813bedaaed47e23d82a778db3485ccd2f
Aleksei Efimov <alefi@mail.ru>
2023-08-21 20:37:47 +0300

    👽️ extend protobuf module with game event cancellation stuff

--- packages/grpc/src/typings/game_service.pb.ts
+++ packages/grpc/src/typings/game_service.pb.ts
@@ -24,6 +24,7 @@ export interface BeginGameEventParamsDto {
 
 export interface EndGameEventParamsDto {
   id: string;
+  cancellationReason?: string | undefined;
 }
 
 export interface GameEventDto {
