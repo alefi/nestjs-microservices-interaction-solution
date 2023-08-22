aa5f5fe783c47aad8c4aa41131fd5a656d36728c
Aleksei Efimov <alefi@mail.ru>
2023-08-22 23:39:15 +0300

    🗃️ rename winningHash property

--- packages/db/prisma/migrations/20230822131358_add_game_session_won_hash_and_game_event_session_count_limit/migration.sql
+++ packages/db/prisma/migrations/20230822131358_add_game_session_won_hash_and_game_event_session_count_limit/migration.sql
@@ -2,4 +2,4 @@
 ALTER TABLE "gaming"."game_events" ADD COLUMN     "sessions_count_limit" INTEGER;
 
 -- AlterTable
-ALTER TABLE "gaming"."game_sessions" ADD COLUMN     "won_hash" TEXT;
+ALTER TABLE "gaming"."game_sessions" ADD COLUMN     "winning_hash" TEXT;
