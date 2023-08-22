24d5b9609420ba471a4d366b439a29285352d18b
Aleksei Efimov <alefi@mail.ru>
2023-08-22 13:59:27 +0300

    🗃️ seed wallet entries database (top-up test user balances), 🐛 fix mess with entity identifiers while database seeding

--- packages/db/contrib/seed.ts
+++ packages/db/contrib/seed.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-import { AuthMethod, Currency, PrismaClient } from '@prisma/client';
+import { AuthMethod, Currency, PrismaClient, Status } from '@prisma/client';
 import {
   game1Id,
   game2Id,
@@ -9,7 +9,20 @@ import {
   walletAccount1Id,
   walletAccount2Id,
   walletAccount3Id,
+  walletAccount1Entry1Id,
+  walletAccount2Entry1Id,
+  walletAccount3Entry1Id,
 } from './identifiers.constants.json';
+
+const buildUrn = ({
+  service = 'top_up_service',
+  type = 'earning',
+  id,
+}: {
+  service?: string;
+  type?: string;
+  id: string;
+}): string => `urn:${service}:${type}:${id}`;
 
 const seedGamesDb = async (prisma: PrismaClient) => {
   await prisma.game.upsert({
@@ -55,6 +68,7 @@ const seedUsersWithWalletAccountsDb = async (prisma: PrismaClient) => {
           {
             where: { id: walletAccount1Id },
             create: {
+              id: walletAccount1Id,
               currency: Currency.RUR,
               isAvailable: true,
               createdAt: faker.date.past(),
@@ -64,6 +78,7 @@ const seedUsersWithWalletAccountsDb = async (prisma: PrismaClient) => {
           {
             where: { id: walletAccount2Id },
             create: {
+              id: walletAccount2Id,
               currency: Currency.USD,
               isAvailable: false,
               createdAt: faker.date.past(),
@@ -93,6 +108,7 @@ const seedUsersWithWalletAccountsDb = async (prisma: PrismaClient) => {
         connectOrCreate: {
           where: { id: walletAccount3Id },
           create: {
+            id: walletAccount3Id,
             currency: Currency.USD,
             isAvailable: true,
             createdAt: faker.date.past(),
@@ -108,6 +124,77 @@ const seedUsersWithWalletAccountsDb = async (prisma: PrismaClient) => {
   console.info('Users and wallet_accounts databases seeded');
 };
 
+const seedWalletEntriesDb = async (prisma: PrismaClient) => {
+  await prisma.walletEntry.upsert({
+    where: { id: walletAccount1Entry1Id },
+    update: {},
+    create: {
+      id: walletAccount1Entry1Id,
+      reference: buildUrn({ id: 'da105576-5f74-44a1-9320-3640ece95cdd' }),
+      amount: 10_500.0,
+      currency: Currency.RUR,
+      status: Status.success,
+      postedAt: new Date(),
+      walletAccount: {
+        connect: {
+          userId_currency: {
+            currency: Currency.RUR,
+            userId: user1Id,
+          },
+        },
+      },
+      createdAt: faker.date.past(),
+      updatedAt: new Date(),
+    },
+  });
+  await prisma.walletEntry.upsert({
+    where: { id: walletAccount2Entry1Id },
+    update: {},
+    create: {
+      id: walletAccount2Entry1Id,
+      reference: buildUrn({ id: '7917d967-d8f9-45a9-a2b2-79de8e2bd9d3' }),
+      amount: 500.5,
+      currency: Currency.USD,
+      status: Status.success,
+      postedAt: new Date(),
+      walletAccount: {
+        connect: {
+          userId_currency: {
+            currency: Currency.USD,
+            userId: user1Id,
+          },
+        },
+      },
+      createdAt: faker.date.past(),
+      updatedAt: new Date(),
+    },
+  });
+  await prisma.walletEntry.upsert({
+    where: { id: walletAccount3Entry1Id },
+    update: {},
+    create: {
+      id: walletAccount3Entry1Id,
+      reference: buildUrn({ id: 'def0cebe-64e7-40a3-92e9-816f22850af4' }),
+      amount: 253.3,
+      currency: Currency.USD,
+      status: Status.success,
+      postedAt: new Date(),
+      walletAccount: {
+        connect: {
+          userId_currency: {
+            currency: Currency.USD,
+            userId: user2Id,
+          },
+        },
+      },
+      createdAt: faker.date.past(),
+      updatedAt: new Date(),
+    },
+  });
+
+  console.info('User wallet account entries database seeded');
+};
+
 void (async () => {
   // It mustn't run on production environment.
   if (process.env.NODE_ENV === 'production') {
@@ -116,4 +203,5 @@ void (async () => {
 
   const prisma = new PrismaClient();
   await Promise.all([seedGamesDb(prisma), seedUsersWithWalletAccountsDb(prisma)]);
+  await seedWalletEntriesDb(prisma);
 })();
