1af1701409d67100fe4cec73b14978237d4e6238
Aleksei Efimov <alefi@mail.ru>
2023-08-24 15:50:57 +0300

    🎨 improve api doc by adding currency enum type

--- apps/gw-web/src/game/session/bid/dto/shared/currency-amount.dto.ts
+++ apps/gw-web/src/game/session/bid/dto/shared/currency-amount.dto.ts
@@ -1,11 +1,18 @@
-import { IsNumberString, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNumberString } from 'class-validator';
 
+import { Currency } from '@lib/db';
 import { type StructV1 } from '@lib/grpc';
 
 export class CurrencyAmountDto implements StructV1.CurrencyAmountDto {
-  @IsString()
+  @ApiProperty({
+    enum: Currency,
+    example: Currency.USD,
+  })
+  @IsEnum(Currency)
   readonly currency: string;
 
+  @ApiProperty({ example: '100.55' })
   @IsNumberString()
   readonly amount: string;
 }
