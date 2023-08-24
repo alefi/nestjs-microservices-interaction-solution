733766e85d2715bf72831caf2cfb54e6d569da82
Aleksei Efimov <alefi@mail.ru>
2023-08-24 11:27:53 +0300

    🩹 fix import of dto in game service

--- apps/game/src/game/bid/game-bid-grpc.controller.ts
+++ apps/game/src/game/bid/game-bid-grpc.controller.ts
@@ -3,14 +3,13 @@ import { GrpcMethod, GrpcService } from '@nestjs/microservices';
 import { GameServiceV1 } from '@lib/grpc';
 import { GameBidDto } from './dto';
 import { GameBidService } from './game-bid.service';
-import { ApplyBidParamsDto } from '@lib/grpc/src/typings/game_service.pb';
 
 @GrpcService()
 export class GameBidGrpcController {
   constructor(private readonly gameBidService: GameBidService) {}
 
   @GrpcMethod('GameService')
-  async applyBid(applyBidParams: ApplyBidParamsDto): Promise<GameServiceV1.GameBidDto> {
+  async applyBid(applyBidParams: GameServiceV1.ApplyBidParamsDto): Promise<GameServiceV1.GameBidDto> {
     const bid = await this.gameBidService.applyBid(applyBidParams);
     return GameBidDto.fromModel(bid);
   }
