1afa4c8504ba1e32effdfbb896ec1e5f44f5614e
Aleksei Efimov <alefi@mail.ru>
2023-08-30 13:29:13 +0300

    🚑 fix bug with delayed jobs

--- packages/queue/src/queue-publisher.service.ts
+++ packages/queue/src/queue-publisher.service.ts
@@ -7,8 +7,8 @@ export class QueuePublisherService implements OnModuleDestroy {
   protected queue: Queue;
   private readonly logger = new Logger(QueuePublisherService.name, { timestamp: true });
 
-  async publish(name: JobName, data: unknown, options?: JobsOptions) {
-    const job = await this.queue.add(name, data, (options = {}));
+  async publish(name: JobName, data: unknown, options: JobsOptions = {}) {
+    const job = await this.queue.add(name, data, options);
     this.logger.debug(
       `Published ${name}:${job.id} along both with data ${JSON.stringify(data)} and options ${JSON.stringify(options)}`,
     );
